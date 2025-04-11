f1f4e8c268f810e2460f0b8d21545c519ed79126
Yunus Yesilcimen <byunusyesilcimen@gmail.com>
2025-04-11 21:16:56 +0300

    Fluent Validation

---
+++ ApiProjeKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,20 @@
+﻿using ApiProjeKampi.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProjeKampi.WebApi.ValidationRules
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Ürün Adını Boş Geçmeyin");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("En az 2 karakter girişi yapınız!");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("En Fazla 50 karakter girişi yapabilirsiniz!");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Ürün Fiyatı Boş Geçilemez!").LessThan(0).WithMessage("Ürün fiyatı negatif olamaz!")
+                .GreaterThan(0).WithMessage("Ürün Fiyatı bu kadar yüksek olamaz, girdiğiniz değeri kontrol ediniz!");
+
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Ürün Açıklaması Boş Geçilemez!");
+        }
+    }
+}
