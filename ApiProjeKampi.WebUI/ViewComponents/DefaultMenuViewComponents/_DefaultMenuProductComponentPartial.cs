1e876fd50521c36cde6c83053ba013c2a43a572c
Yunus Yesilcimen <byunusyesilcimen@gmail.com>
2025-04-26 17:32:40 +0300

    productcomponentpartial

--- ApiProjeKampi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductComponentPartial.cs
+++ ApiProjeKampi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductComponentPartial.cs
@@ -1,14 +1,14 @@
-﻿using ApiProjeKampi.WebUI.Dtos.CategoryDtos;
+﻿using ApiProjeKampi.WebUI.Dtos.ProductDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace ApiProjeKampi.WebUI.ViewComponents.DefaultMenuViewComponents
 {
-    public class _DefaultMenuCategoryComponentPartial:ViewComponent
+    public class _DefaultMenuProductComponentPartial:ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _DefaultMenuCategoryComponentPartial(IHttpClientFactory httpClientFactory)
+        public _DefaultMenuProductComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
@@ -16,11 +16,11 @@ namespace ApiProjeKampi.WebUI.ViewComponents.DefaultMenuViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7214/api/Categories/");
+            var responseMessage = await client.GetAsync("https://localhost:7214/api/Products/");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
                 return View(values);
             }
             return View();
